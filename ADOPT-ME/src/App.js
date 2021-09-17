4e5e9edd6d08598bf630b78118394c915711c24b
Silvia Herrera <silviaherrera33@gmail.com>
2021-09-16 23:11:55 -0500

    Eslint config and React-specifics

--- ADOPT-ME/src/App.js
+++ ADOPT-ME/src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", { id: "something-important" }, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-      name: "Doink",
-      animal: "Cat",
-      breed: "Mixed",
-    }),
-  ]);
+return (
+    <div>
+        <h1 id="something-important">Adopt me!</h1>
+        <Pet name="Luna" animal="Dog" breed="Havanese" />
+        <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+        <Pet name="Doink" animal="Cat" breed="Mixed" />
+    </div>
+)
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
